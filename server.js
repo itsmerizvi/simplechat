330a3d99ccb922c8cc362ca0831a1c3414d1b144
Mohammed Rizvi <mohammedrizvi@Mohammeds-MacBook-Air.local>
2023-08-23 11:57:04 +0530

    emoji update

--- server.js
+++ server.js
@@ -6,20 +6,45 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+let users = [];
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('add user', (userData) => {
+        const existingUser = users.find(user => user.name === userData.name);
+        if (!existingUser) {
+            users.push({
+                id: socket.id,
+                name: userData.name,
+                color: userData.color
+            });
+
+            socket.userName = userData.name;  // Set the userName for this socket
+            console.log('Emitting updated user list after user addition:', users);  // Debug log
+            io.emit('users list', users);
+        }
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        // Remove the user from the list
+        users = users.filter(user => user.id !== socket.id);
+        console.log('Emitting updated user list after user disconnect:', users);  // Debug log 
+        io.emit('users list', users);
     });
+
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+        console.log("Received message from client:", msg);
+        socket.broadcast.emit('chat message', msg); // Send to everyone except the sender
     });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
+
